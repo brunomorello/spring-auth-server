8a9d67b3792fee3c64512a4320b8e3f67c33bb2d
Bruno Moreno <brunomorello7@gmail.com>
2023-09-09 17:15:26 +0100

    test CORS fix

--- src/main/java/pt/com/bmo/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/pt/com/bmo/authserver/config/AuthorizationServerConfig.java
@@ -110,7 +110,7 @@ public class AuthorizationServerConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 //        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("http://localhost:[*]", "https://*.herokuapp.com:[*]"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://list4u-front.herokuapp.com/"));
+        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
